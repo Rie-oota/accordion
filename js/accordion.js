20fed69fb6db3b3668469ac1dc4844d1d7f66ac6
Yoshida <sunnri257@gmail.com>
2025-06-17 19:56:11 +0900

    修正

--- js/accordion.js
+++ js/accordion.js
@@ -1,13 +1,13 @@
 $(function(){
-// タブのクリック時に実行する処理
-$(".toggleIcon").on("click", function() {
-// アイコンのclass属性を切り替え
-$(this).toggleClass("active");
-// DOMの中から.titleをすべて取得
-const titleArray = document.querySelectorAll(".title");
-// .toggleIconのデータ属性を取得
-const index = this.dataset.listIndex
-// titleArrayからDOMを取得し、兄弟要素のアコーディオンを開閉する
-$(titleArray[index]).next().slideToggle()
-});
+  // タブのクリック時に実行する処理
+  $(".toggleIcon").on("click", function() {
+    // アイコンのclass属性を切り替え
+    $(this).toggleClass("active");
+    // DOMの中から.titleをすべて取得
+    const titleArray = document.querySelectorAll(".title");
+    // .toggleIconのデータ属性を取得
+    const index = this.dataset.listIndex
+    // titleArrayからDOMを取得し、兄弟要素のアコーディオンを開閉する
+    $(titleArray[index]).next().slideToggle()
+  });
 });
